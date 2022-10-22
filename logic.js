c83d2604a37a96c9e646b8eb606753d2a1c1c943
Prabhash Rai <prabhashsahaj@gmail.com>
2022-10-23 00:07:41 +0530

    created seperate water bar

--- logic.js
+++ logic.js
@@ -11,23 +11,22 @@ function debounce(func, delay) {
 let array;
 const inputChanged = debounce(() => {
     array = document.getElementById("array").value;
-    // stored(array)
-    stored("12, 40, 5, 10, 3, 36")
+    stored(array)
 }, 1000)
 
 let water_stored = [], arr = [], max_height = 0;
 
 function stored(input_array) {
 
-    arr = input_array.split(",");
+  arr = input_array.split(",");
     const size = arr.length;
 
     for (let i=0; i<size; i++) {
-        arr[i] = arr[i].trim();
-        arr[i] = +arr[i];
+      arr[i] = arr[i].trim();
+      arr[i] = +arr[i];
 
-        max_height = Math.max(max_height, arr[i]);
-        if (Number.isNaN(arr[i])) console.error("only integers are allowed in array");
+      max_height = Math.max(max_height, arr[i]);
+      if (Number.isNaN(arr[i])) console.error("only integers are allowed in array");
     }
 
     let prefix = [...arr];
@@ -46,9 +45,8 @@ function stored(input_array) {
         water_stored[i] = Math.min(prefix[i], suffix[i]) - arr[i];
     }
 
-    console.log(water_stored);
     drawChart(arr, document.getElementById('given_graph'));
-    drawChart(water_stored, document.getElementById('graph'));
+    drawChart(water_stored, document.getElementById('graph'), true, arr);
 }
 
 window.onresize = displayWindowSize;
@@ -56,10 +54,37 @@ window.onload = displayWindowSize;
 
 function displayWindowSize() {
   drawChart(arr, document.getElementById('given_graph'));
-  drawChart(water_stored, document.getElementById('graph'), true);
+  drawChart(water_stored, document.getElementById('graph'), true, arr);
 };
 
-function drawChart(data, element, water = false) {
+function drawChart(data, element, water = false, originalArr = []) {
+  if (water) {
+    drawWaterChart(data, element, originalArr);
+    return;
+  }
+
+  let graph = element;
+  
+  const height = window.innerHeight*0.5;
+  const width = window.innerWidth*0.7;
+  graph.style.height = height;
+  graph.style.width = width;
+
+  const height_ratio = height / (max_height + 5);
+
+  let chart = '';
+  let x = 0;
+
+  for (let i=0; i<data.length; i++) {
+    chart += '<rect x ="' + x + '" y ="' 
+      + (height - (data[i]*height_ratio)) 
+      + '" width = "'+ (width / data.length) +'" height ="' + (data[i]*height_ratio) + '" />';
+    x+= (width / data.length);
+  }
+  graph.innerHTML = chart;
+}
+
+function drawWaterChart(data, element, arr) {
   let graph = element;
   
   const height = window.innerHeight*0.5;
@@ -69,14 +94,12 @@ function drawChart(data, element, water = false) {
 
   const height_ratio = height / (max_height + 20);
 
-  console.log(height_ratio)
-
   let chart = '';
   let x = 0;
 
   for (let i=0; i<data.length; i++) {
     chart += '<rect x ="' + x + '" y ="' 
-      + (height - (data[i]*height_ratio)) 
+      + (height - ((data[i] + arr[i])*height_ratio)) 
       + '" width = "'+ (width / data.length) +'" height ="' + (data[i]*height_ratio) + '" />';
     x+= (width / data.length);
   }
